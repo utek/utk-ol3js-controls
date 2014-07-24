d0c1f895c6f46885b67bee1f56b0894afa2e1322
Łukasz Bołdys <mail@utek.pl>
2014-07-24 13:54:07 +0200

    cleanup

--- src/js/utk-controls.js
+++ src/js/utk-controls.js
@@ -1,29 +1,26 @@
 var utk = utk || {};
-
-utk.Legend = function(opt_options) {
-
-  var options = opt_options || {elements: []};
+utk.Legend = function (opt_options) {
+  var options = opt_options || {
+    elements: []
+  };
   var elements = [];
   for (var i = 0; i < options.elements.length; i++) {
     var div = document.createElement('div');
-    div.innerHTML = '<span class="'+options.elements[i].class+
-    ' utk-legend-element">&nbsp;&nbsp;</span><strong>'+options.elements[i].name+'</strong>';
+    div.innerHTML = '<span class="' + options.elements[i].class +
+      ' utk-legend-element">&nbsp;&nbsp;</span><strong>' + options.elements[i]
+      .name + '</strong>';
     elements.push(div);
   }
-
   var this_ = this;
-
   /* creating legend box */
   var element = document.createElement('div');
   element.className = 'utk-legend ol-unselectable';
   for (i = 0; i < elements.length; i++) {
     element.appendChild(elements[i]);
   }
-
   ol.control.Control.call(this, {
     element: element,
     target: options.target
   });
-
 };
 ol.inherits(utk.Legend, ol.control.Control);
